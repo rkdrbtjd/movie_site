1cb848c24a08b4fd5a660538e9806c9cf544f2aa
rkdrbtjd <72647202+rkdrbtjd@users.noreply.github.com>
2024-12-07 23:42:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ def main():
                   existing_rating = ratings_df[
                     (ratings_df["username"] == st.session_state.user) &
                     (ratings_df["movie"] == movie['title'])
-                ]
+                    ]
                 if not existing_rating.empty:
                     st.info("이미 이 영화에 평점을 남겼습니다.")
                 else:
